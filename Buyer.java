da022940aeffa7ed2c09be8eb13f8cf4dbe7e67e
mr.debbie <giyoungchong1980@gmail.com>
2017-02-06 21:59:17 -0600

    Add void method to add an Item

--- Buyer.java
+++ Buyer.java
@@ -22,7 +22,14 @@ public Buyer(Department dept, PurchaseOrder pOrder){
  public PurchaseOrder getPurchaseOrder(){
   //return copy of purchase object
   return new PurchaseOrder(purchase);
- }
+}
+ public void addItem(String n, int q, double p){
+   /*Item item = new Item (n,q,p);
+   purchase.orderDetail(item);
+   return item;*/
+   ArrayList<Item> itemList = new ArrayList<Item>();
+   itemList.add(new Item(n, q, p));
+}
  public String toString(){
    String str = "\nDepartment Info:\n" + department + "\nPurchase Order Info:\n" +
    purchase;
